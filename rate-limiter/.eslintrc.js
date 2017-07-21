d98ced90f63c984730e3297b67ce1954c59f5f38
Michael Pardue <mpardue@exzeo.com>
2017-07-21 08:54:14 -0400

    configure testing and linting

---
+++ rate-limiter/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 2017
+  },
+  "extends": "airbnb-base",
+  "rules": {
+    "comma-dangle": [2, "never"],
+    "consistent-return": [0],
+    "arrow-body-style": [1, "as-needed"],
+    "import/no-unresolved": [0],
+    "strict": [0, "global"]
+  }
+};
